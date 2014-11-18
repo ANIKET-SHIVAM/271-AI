dcbc3c028ce55ed2e2fa721b8058f45f613dcb1c
ramb0x <hiten.bhakta@gmail.com>
2014-11-18 10:31:05 -0800

    Board Parser
    Project for parsing the game board, to be used by AI.

---
+++ opencv.hpp
@@ -0,0 +1,47 @@
+#ifndef OPENCV_HPP
+#define OPENCV_HPP
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <fstream>
+#include <opencv2/core/utility.hpp>
+#include "opencv2/objdetect.hpp"
+#include "opencv2/imgproc.hpp"
+#include <sstream>
+#include <queue>
+#include <ostream>
+#include <vector>
+#pragma comment(lib, "vfw32.lib")
+#define SSTR( x ) dynamic_cast< std::ostringstream & >( \
+        ( std::ostringstream() << std::dec << x ) ).str()
+
+using namespace cv;
+using namespace std;
+void MatchingMethod( int, void* );
+void thisMouseCallBack(int event, int x, int y, int flags, void *param);
+void maxLocs(const Mat& src, queue<Point>& dst, size_t size);
+//Global vars
+Mat img;
+Mat templ;
+Mat result;
+char* image_window = "Source Image";
+char* result_window = "Result window";
+int match_method;
+int max_Trackbar = 5;
+int lowT;
+char GAME_STATE[15][15];
+queue<Point> locations;
+char L;
+Mat mx;
+string ADB_DIR("C:/adt/sdk/platform-tools/");
+class RGB {
+public:
+    uchar R;
+    uchar G;
+    uchar B;
+    RGB(uchar R,uchar G, uchar B) : R(R), G(G), B(B) {}
+};
+#endif
